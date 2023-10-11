5b9b5a4ba67599c55d6e448650eac5bb99b1cad4
prabhpreetS <prabhpreetsingh1707@gmail.com>
2023-10-11 19:35:12 +0530

    full app.py

--- app.py
+++ app.py
@@ -96,6 +96,40 @@ def extract_clips():
 
     return multimedia
 
+@app.route('/timings', methods=['GET', 'POST'])
+def timings():
+    if request.method == 'POST':
+        video_filename = request.form['video_filename']
+        start_time = request.form['start_time']
+        end_time = request.form['end_time']
+
+        # Convert start_time and end_time to float values
+        start_time = float(start_time)
+        end_time = float(end_time)
+
+        # Construct the video path and audio path
+        video_path = os.path.join('static', 'upload', video_filename)
+        audio_path = os.path.join('static', 'upload', os.path.splitext(video_filename)[0] + '.wav')
+
+        # Load the video clip using moviepy
+        clip = mp.VideoFileClip(video_path)
+
+        # Set the start and end times for cropping
+        clip = clip.subclip(start_time, end_time)
+
+        # Save the cropped video clip to a new file
+        cropped_filename = os.path.splitext(video_filename)[0] + '_cropped.mp4'
+        cropped_path = os.path.join('static', 'upload', cropped_filename)
+        clip.write_videofile(cropped_path)
+
+        return jsonify({'status': 'success', 'message': 'Video cropped successfully!', 'filename': cropped_filename})
+
+    else:
+        video_filenames = request.form.getlist('video_filenames[]')
+
+        video_files = [f for f in os.listdir('static/upload') if f.endswith('.mp4')]
+        print("video_files");
+        return render_template('timings.html', video_filenames=video_files)
 
 
 if __name__ == '__main__':
